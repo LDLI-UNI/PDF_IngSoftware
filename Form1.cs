4ab8ebd43cbc6b2d3158660cc590986ba5ac05ac
LDLI-UNI <luis.lopezi35@my.unitec.edu.mx>
2024-04-09 14:25:24 -0600

    primer commit

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace TransformarPDF
+{
+    class Program
+    {
+        public static StringBuilder stringbuilder = new StringBuilder();
+        public static StringBuilder StringBuilder { get; private set; }
+
+        static void Main(string[] args, StringBuilder stringbuilder)
+        {
+            String ElArchivo = @"C:\Users\wkldl\Downloads\List_of_dielectric_constants_EN.pdf";
+            using (PdfReader reader = new PdfReader(ElArchivo))
+            {
+                for (int pag = 1; pag <= reader.NumberOfPages; pag++)
+                {
+                    ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                    string text = PdfTextExtractor.GetTextFromPage(reader, pag, strategy);
+                    text = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes.(text)))stringbuilder = stringbuilder =sb.Append(text);
+
+                }
+            }
+            Console.WriteLine(sb.ToString());
+            Console.ReadKey();
+        }
+    }
+}
